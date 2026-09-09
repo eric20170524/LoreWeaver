8f59959fcb930a09061fc368eb6a77df3bedb7a9
eric20170524 <18995620052@163.com>
2026-09-09 14:27:07 +0800

    feat(survivor): export run growth milestones modifier

--- minigame_master/core/lib/gameplay/survivor_horde/index.js
+++ minigame_master/core/lib/gameplay/survivor_horde/index.js
@@ -80,6 +80,10 @@ export {
     WEAPON_STANCE_CYCLE_DEFAULT_CONFIG
 } from './modifiers/WeaponStanceCycleModifier.js';
 export {
+    default as RunGrowthMilestonesModifier,
+    RUN_GROWTH_MILESTONES_DEFAULT_CONFIG
+} from './modifiers/RunGrowthMilestonesModifier.js';
+export {
     SURVIVOR_HORDE_MODIFIER_REGISTRY,
     SURVIVOR_HORDE_SUPPORTED_MODIFIERS,
     createSurvivorHordeModifier,
